0e0581c4fa14ea4bdc65fe65c6533c01b445425d
Betty <betty@lytana.net>
2016-11-21 20:10:59 +0100

    Added node versionin packag.jso and const port in servern

--- server.js
+++ server.js
@@ -1,13 +1,12 @@
 var express  = require('express');
 
 //Create our app
-
 var app = express();
 
 const PORT = process.env.PORT || 3000;
 
 app.use(function(req, res, next){
-	if(req.headers['x-forwarded-proto'] === 'http') {
+	if(req.protocol === 'http') {
 		next();
 	} else {
 		res.redirect('http://' + req.hostname + req.url);
