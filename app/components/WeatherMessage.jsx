4560ce87ae59283d0f718898c01144f85d2ec454
Betty <betty@lytana.net>
2016-11-21 21:11:15 +0100

    Converting fahrenheit to celsius

--- app/components/WeatherMessage.jsx
+++ app/components/WeatherMessage.jsx
@@ -3,7 +3,7 @@ var React = require('react');
 var WeatherMessage = ({temp, location}) => {
 
 	return (
-		<h3>It is {Math.round((temp - 32) * 5 /9 )} degree celsius in {location}!</h3>
+		<h3>It is {Math.round((temp - 32) * 5 /9 )} degree celsius in {location}.</h3>
 	);
 };
 
